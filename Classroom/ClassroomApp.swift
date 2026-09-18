34d4749b12daa131227f6eb419ccac8887af3d0d
posteronework <posteronework@gmail.com>
2026-09-18 17:48:15 +0200

    Initial Commit

---
+++ Classroom/ClassroomApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClassroomApp.swift
+//  Classroom
+//
+//  Created by Никита Иванов on 9/18/26.
+//
+
+import SwiftUI
+
+@main
+struct ClassroomApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
